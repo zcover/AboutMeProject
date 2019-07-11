cf85d36362b2667c06e71ccb5f9eac634d19d845
adrianhuebner <adrianh92@gmail.com>
2019-07-11 16:00:14 -0700

    line 44 edit

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
 'use strict';
 
-// Function 'gathername' defined and then verify expected response, will loop until provided
-function gathername(){   
-   var myName = prompt('What is my name?')
-    myName = myName.toLowerCase()
-    //Below is the loop for correct answer
-    if (myName.toLowerCase() === 'zerek'){
-        alert('You are correct!');
-    } else {
-                alert('Noooope, try again!');
-                gathername()
-            }
-}
+// // Function 'gathername' defined and then verify expected response, will loop until provided
+// function gathername(){   
+//    var myName = prompt('What is my name?')
+//     myName = myName.toLowerCase()
+//     //Below is the loop for correct answer
+//     if (myName.toLowerCase() === 'zerek'){
+//         alert('You are correct!');
+//     } else {
+//                 alert('Noooope, try again!');
+//                 gathername()
+//             }
+// }
 
-// gathername();
+// // gathername();
 
-function wrapUp(){
-    var noPrompt = prompt("Wowza! You're good at this! Figured out who I am yet?")
-    if (noPrompt.toLowerCase() === 'no'){
-        alert('Too bad!')
-    }
-}
-wrapUp();
+// function wrapUp(){
+//     var noPrompt = prompt("Wowza! You're good at this! Figured out who I am yet?")
+//     if (noPrompt.toLowerCase() === 'no'){
+//         alert('Too bad!')
+//     }
+// }
+// wrapUp();
 
-// Function 'changepage' defined to change background color
-function changepage(){
-    var yourName = prompt('Winner, what is YOUR name?')
-    var changeColor = prompt(yourName + ' Now has access to admin controls. Enter prefered text color.');
+// // Function 'changepage' defined to change background color
+// function changepage(){
+//     var yourName = prompt('Winner, what is YOUR name?')
+//     var changeColor = prompt(yourName + ' Now has access to admin controls. Enter prefered text color.');
 
-    //This line actively replaces h1 text to add reward system
-    document.getElementById("h1edit").innerHTML = (yourName + ' successfully guessed my name!');
+//     //This line actively replaces h1 text to add reward system
+//     document.getElementById("h1edit").innerHTML = (yourName + ' successfully guessed my name!');
 
-    //This line actively changes the css style to change most text color per user input
-    document.body.style.color = (changeColor);
-}
-// Defines 'runpage' function, which will hold entire page js actions at this time
-function runpage(){
-    gathername();
-    changepage()
-}
-// runpage is the only active script on this page, relying on previous 3 functions to gather variables
-runpage();
-
+//     //This line actively changes the css style to change most text color per user input
+//     document.body.style.color = (changeColor);
+// }
+// // Defines 'runpage' function, which will hold entire page js actions at this time
+// function runpage(){
+//     gathername();
+//     changepage()
+// }
+// // runpage is the only active script on this page, relying on previous 3 functions to gather variables
+// runpage();
+// EVERYTHING ABOVE THIS WORKS!!!
 
 //Below is scripting for Lab 3
 // var wordbank = ['Zerek', 'fish', 'other things']
